ee4b0eda5666da72250f6033ee7cc520f1ebb4ed
Enoch2090 <gyc990926@gmail.com>
2020-06-24 16:01:36 +0800

    hand.cpp, player.cpp

---
+++ Project3/hand.cpp
@@ -0,0 +1,41 @@
+#include "hand.h"
+Hand::Hand()
+{
+    this->curValue.count = 0;
+    this->curValue.soft = true;
+}
+
+void Hand::discardAll()
+{
+    this->curValue.count = 0;
+    this->curValue.soft = false;
+}
+
+void Hand::addCard(Card c) // TODO: change to soft?
+{
+    if (c.spot <= 8)
+    {
+        this->curValue.count += (c.spot + 2);
+    }
+    else if (c.spot > 8 && c.spot < 12)
+    {
+        this->curValue.count += (10);
+    }
+    else if (c.spot == 12)
+    {
+        if (this->curValue.count > 10)
+        {
+            this->curValue.count += 1;
+        }
+        else
+        {
+            this->curValue.count += 11;
+            this->curValue.soft = true;
+        }
+    }
+    if (this->curValue.count > 21 && this->curValue.soft)
+    {
+        this->curValue.count -= 10; // Change a soft ACE to a hard ACE.
+        this->curValue.soft = false;
+    }
+}

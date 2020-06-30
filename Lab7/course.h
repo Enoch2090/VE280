a044eaee836503cb9fa4f62d245372badc3e90a9
Enoch2090 <gyc990926@gmail.com>
2020-06-30 11:11:01 +0800

    Lab7

---
+++ Lab7/course.h
@@ -0,0 +1,23 @@
+//
+// VE280 Lab 7, SU2020
+// Created by cyx on 20-5-22.
+//
+
+#ifndef COURSE_H
+#define COURSE_H
+
+// an exception class
+class tooManyTasks{};
+
+class Course{
+public:
+    // TODO: declare methods and destructor here
+};
+
+Course *create(const std::string &class_type, const std::string &course_code, bool assign_size, int tasks_size);
+// EFFECTS: returns a pointer according to class_type
+//          If class_type is not "Technical" or "Upper Level Technical", returns a null pointer.
+//          If assignSize is true, the maximum number of tasks is specified by tasksSize;
+//          otherwise, the maximum number of tasks is the default value.
+
+#endif //COURSE_H
